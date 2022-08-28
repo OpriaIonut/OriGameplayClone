634f8811f6d88c3621cb6e11519792806a35cb23
Ionut Opria <opriaionut14@gmail.com>
2022-08-28 15:08:00 +0300

    Improved character movement

--- OriGameplayClone/Assets/_OriGameplayClone/Scripts/CharacterMovement.cs
+++ OriGameplayClone/Assets/_OriGameplayClone/Scripts/CharacterMovement.cs
@@ -9,9 +9,11 @@ public class CharacterMovement : MonoBehaviour
     public float jumpInputTime = 1.0f; //For the first jump, if we hold they key, we can jump higher. This variable defined how long we can keep pressing
     public float gravity = 15.0f; //The default gravity didn't achieve proper results, so I defined in fixed update a new gravity
     public float midairAcceleration = 10.0f; //In mid-air, if we change direction, we need to accelerate from 0 to a certain speed.
+    public float wallJumpSpeed = 100.0f;
+    public float wallJumpDeacc = 1.0f;
 
     private bool isGrounded = true;     //Did we hit a ground collider?
-    private bool canClimbWall = false;  //Did we hit a climbable wall collider?
+    private bool canWallJump = false;  //Did we hit a climbable wall collider?
     private bool canJump = true;        //Set to true when lifting jump key & false when actually jumping
     private bool canDoubleJump = true;  //Reset when we hit the ground
     private bool holdingSpace = false;  //Used to create the effect of holding jump key to jump higher
@@ -21,6 +23,7 @@ public class CharacterMovement : MonoBehaviour
     private float horizontalInput = 0.0f;
     private float previousMoveDir = 1.0f;   //Used to detect change of direction in mid-air
     private float accelerationFactor = 1.0f;//Goes from 0 to 1 when we change direction in mid-air
+    private float wallJumpPropulsion = 0.0f;//Is used to propell us in an opposite direction to the wall when doing wall jump.
 
     private Rigidbody rb;
 
@@ -44,17 +47,23 @@ public class CharacterMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        float currentMoveDir = horizontalInput > 0.0f ? 1.0f : -1.0f;
+
         //Rotate player to face move direction
         if (horizontalInput != 0.0f)
         {
-            float rotAngle = horizontalInput > 0.0f ? 90.0f : -90.0f;
+            if (currentMoveDir != previousMoveDir)
+                wallJumpPropulsion = 0.0f;
+        }
+        if (rb.velocity.x != 0.0f)
+        {
+            float rotAngle = rb.velocity.x > 0.0f ? 90.0f : -90.0f;
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, rotAngle, transform.rotation.eulerAngles.z);
         }
 
         //Detect if we changed direction in mid-air
         if (!isGrounded)
         {
-            float currentMoveDir = horizontalInput > 0.0f ? 1.0f : -1.0f;
             if (currentMoveDir != previousMoveDir)
             {
                 accelerationFactor = 0.0f;
@@ -62,7 +71,13 @@ public class CharacterMovement : MonoBehaviour
         }
 
         //Apply horizontal movement
-        rb.velocity = new Vector3(moveSpeed * accelerationFactor * horizontalInput * Time.fixedDeltaTime, rb.velocity.y, rb.velocity.z);
+        float xVel = moveSpeed * accelerationFactor * horizontalInput;
+        if (wallDirection != currentMoveDir)
+            xVel += wallJumpPropulsion * wallDirection;
+
+        rb.velocity = new Vector3(xVel * Time.fixedDeltaTime, rb.velocity.y, rb.velocity.z);
+
+        wallJumpPropulsion = Mathf.Clamp(wallJumpPropulsion - wallJumpDeacc * Time.fixedDeltaTime, 0.0f, wallJumpSpeed);
 
         //Retain certain variables used to detect change of direction in mid-air
         previousMoveDir = horizontalInput > 0.0f ? 1.0f : -1.0f;
@@ -72,15 +87,21 @@ public class CharacterMovement : MonoBehaviour
         if (holdingSpace)
         {
             //Check if we should make a new jump
-            if (canJump && (isGrounded || canDoubleJump))
+            if (canJump && (canWallJump || isGrounded || canDoubleJump))
             {
                 float speed = jumpSpeed;
                 float jumpCounterMax = jumpInputTime;
 
                 //Based on if it is first jump or second jump, change the properties
-                if (isGrounded == false)
+                if(!isGrounded && canWallJump)
+                {
+                    wallJumpPropulsion = wallJumpSpeed;
+                    canWallJump = false;
+                }
+                else if (isGrounded == false) //Second jump
                 {
                     speed *= 1.5f;
+                    wallJumpPropulsion = 0.0f;
                     canDoubleJump = false;
                     jumpCounterMax = 0.0f;
                 }
@@ -108,7 +129,8 @@ public class CharacterMovement : MonoBehaviour
     {
         if(other.tag == "ClimbableWall")
         {
-            canClimbWall = true;
+            canWallJump = true;
+            canDoubleJump = true;
             wallDirection = other.transform.position.x - transform.position.x;
             wallDirection /= Mathf.Abs(wallDirection); //Convert it to -1 or 1
         }
@@ -117,6 +139,7 @@ public class CharacterMovement : MonoBehaviour
             isGrounded = true;
             canDoubleJump = true;
             accelerationFactor = 1.0f;
+            wallJumpPropulsion = 0.0f;
         }
         else if(other.tag == "BreakablePlatform")
         {
